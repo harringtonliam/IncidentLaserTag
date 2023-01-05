9caa8a0fa67d889e61a0060888e9c0f1465a5a02
Liam Harrington <harringtonliam@hotmail.com>
2023-01-05 15:37:42 +0000

    Doors Added

---
+++ Assets/Scripts/Scenery/Door.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Scenery
+{
+
+    public class Door : MonoBehaviour
+    {
+        [SerializeField] Transform rightDoor = null;
+        [SerializeField] Transform leftDoor = null;
+        [SerializeField] Transform rightOpenPosition = null;
+        [SerializeField] Transform rightClosedPosition = null;
+        [SerializeField] Transform leftOpenPosition = null;
+        [SerializeField] Transform leftClosedPosition = null;
+        [SerializeField] float doorSpeed = 0.5f;
+
+        bool opening = false;
+        bool closing = false;
+
+
+        private void Update()
+        {
+            if (opening)
+            {
+                OpenDoors();
+            }
+            else if (closing)
+            {
+                CloseDoors();
+            }
+            
+        }
+
+        public void StartOpening()
+        {
+            closing = false;
+            opening = true; 
+        }
+
+        public void StartClosing()
+        {
+            opening = false;
+            closing = true;
+        }
+
+        private void CloseDoors()
+        {
+            rightDoor.position = Vector3.MoveTowards(rightDoor.position, rightClosedPosition.position, doorSpeed * Time.deltaTime);
+            leftDoor.position = Vector3.MoveTowards(leftDoor.position, leftClosedPosition.position, doorSpeed * Time.deltaTime);
+            float distanceToClosed = Vector3.Distance(rightDoor.position, rightClosedPosition.position);
+            if (Mathf.Approximately(distanceToClosed, 0f))
+            {
+                closing = false;
+            }
+        }
+
+        private void OpenDoors()
+        {
+            rightDoor.position = Vector3.MoveTowards(rightDoor.position, rightOpenPosition.position, doorSpeed * Time.deltaTime);
+            leftDoor.position = Vector3.MoveTowards(leftDoor.position, leftOpenPosition.position, doorSpeed * Time.deltaTime);
+            float distanceToOpen = Vector3.Distance(rightDoor.position, rightOpenPosition.position);
+            if (Mathf.Approximately(distanceToOpen, 0f))
+            {
+                opening = false;
+            }
+        }
+
+    }
+
+}
+
+
--- Assets/Scripts/Scenery/ElevatorShaftController.cs
+++ Assets/Scripts/Scenery/ElevatorShaftController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 namespace RPG.Scenery
 
 {
@@ -12,14 +13,15 @@ namespace RPG.Scenery
         [SerializeField] Transform elevator;
         [SerializeField] Transform[] doors;
         [SerializeField] float elevatorSpeed = 1f;
-        [SerializeField] float elevatorPauseTime = 6f;
+        [SerializeField] float elevatorPauseTime = 8f;
+        [SerializeField] float doorCloseTime = 2.5f;
         [SerializeField] float doorDistanceTolerance = 0.1f;
         [SerializeField] int doorIndex = 0;
         [SerializeField] bool isGoingUp = true;
         float timeAtDoor;
 
 
-
+        int currentDoorIndex; 
 
         // Update is called once per frame
         void Update()
@@ -35,17 +37,20 @@ namespace RPG.Scenery
 
             if (AtDoor())
             {
-                Debug.Log("Elevator at door");
                 timeAtDoor = 0;
+                currentDoorIndex = doorIndex;
                 GetNextDoor();
             }
 
-
-
+            if (timeAtDoor > elevatorPauseTime - doorCloseTime)
+            {
+                CloseDoor();
+            }
 
             if (timeAtDoor > elevatorPauseTime)
             {
                 elevator.position = Vector3.MoveTowards(elevator.position, GetPositionToMoveTo(), elevatorSpeed * Time.deltaTime);
+                
             }
 
 
@@ -58,6 +63,7 @@ namespace RPG.Scenery
 
             if (distanceToDoor <= doorDistanceTolerance)
             {
+                OpenDoor();
                 return true;
             }
             else
@@ -91,6 +97,24 @@ namespace RPG.Scenery
         {
             Vector3 positionToMoveTo = new Vector3(elevator.position.x, doors[doorIndex].position.y, elevator.position.z);
             return positionToMoveTo;
+        }
+
+        private void OpenDoor()
+        {
+            Door door = doors[doorIndex].GetComponent<Door>();
+            if (door != null)
+            {
+                door.StartOpening();
+            }
+        }
+
+        private void CloseDoor()
+        {
+            Door door = doors[currentDoorIndex].GetComponent<Door>();
+            if (door != null)
+            {
+                door.StartClosing();
+            }
         }
 
     }
